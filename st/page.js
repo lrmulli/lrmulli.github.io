a2db6e3314f12cee1cb6d0226af47339025254a2
lrmulli <lmullineux@gmail.com>
2022-09-09 14:57:09 +0100

    skeleton

--- st/page.js
+++ st/page.js
@@ -3,4 +3,32 @@ var patToken = "";
 document.querySelector('#authform').addEventListener('submit', function(event) {
     patToken = document.querySelector('#patToken').value
     //alert(patToken)
+    getDevices()
 });
+
+function getDevices()
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", "https://api.smartthings.com/v1/devices/", true); // true for asynchronous 
+    xmlhttp.setRequestHeader("Authorization", "Bearer "+patToken);
+    xmlHttp.send(null);
+}
+function receiveDevices(response)
+{
+ console.log(response)
+}
+
+function httpGetAsync(theUrl, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    xmlHttp.open("GET", theUrl, true); // true for asynchronous 
+    xmlHttp.send(null);
+}
